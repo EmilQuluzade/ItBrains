594b8e15fa7e4e9bb243195d1f0fbaeb76e36540
thedipi <dipiland777@gmail.com>
2023-08-21 16:34:45 +0400

    Finished tasks for lesson 8

---
+++ Lesson 8/Lesson 8/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Lesson_8
+{
+    internal class Program
+    {
+        private static Student[] students;
+        static void Main(string[] args)
+        {
+            students = new Student[]
+            {
+                new Student("Aysel", 'A', 18),
+                new Student("Nijat", 'B', 20),
+                new Student("Samir", 'C', 22),
+                new Student("Sevda", 'A', 19),
+                new Student("Tural", 'B', 21),
+                new Student("Leyla", 'C', 23),
+                new Student("Seymur", 'A', 18),
+                new Student("Elmira", 'B', 20),
+                new Student("Arif", 'C', 22),
+                new Student("Aysu", 'A', 19),
+                new Student("Ayaz", 'B', 21),
+                new Student("Aynur", 'C', 23),
+                new Student("Vugar", 'A', 18),
+                new Student("Gunay", 'B', 20),
+                new Student("Narmin", 'C', 22),
+                new Student("Namiq", 'A', 19),
+                new Student("Aygul", 'B', 21),
+                new Student("Turkan", 'C', 23),
+                new Student("Togrul", 'A', 18),
+                new Student("Nigar", 'B', 20)
+            };
+            void GetBirthYear()
+            {
+                Console.WriteLine(" Whose birthday do you want to check?");
+                string studentName = Console.ReadLine();
+                bool found = false;
+                foreach (var item in students)
+                {
+                    if (item.Name == studentName)
+                    {
+                        Console.WriteLine($"Birth year is {2023 - item.Age}");
+                        found = true;
+                        break;
+                    }
+                    
+                }
+
+                if (!found) 
+                {
+                    Console.WriteLine("Wrong Name");
+                        }
+
+            }
+
+            void CheckStudentCount()
+            {
+                Console.WriteLine("Enter group letter you want to check (A, B, C)");
+                var groupName = Console.ReadLine();
+
+                int groupCount = 0; 
+
+                foreach (var student in students)
+                {
+                    if (student.Group == groupName[0])
+                    {
+                        groupCount++;
+                    }
+                }
+
+                if (groupCount > 0)
+                {
+                    Console.WriteLine($"Number of students in group {groupName}: {groupCount}");
+                }
+                else
+                {
+                    Console.WriteLine($"No students found in group {groupName}");
+                }
+            }
+
+
+
+
+
+
+
+            while (true)
+            {
+                Console.WriteLine("1. Get student's birthday");
+                Console.WriteLine("2. Get how many students are there in each group");
+                Console.WriteLine("3.Exit");
+                Console.WriteLine("Enter your choice by number");                
+                int inputChoice = Convert.ToInt32(Console.ReadLine());
+
+                switch (inputChoice)
+                {
+                    case 1:
+                        GetBirthYear();
+                        break;
+                        case 2:
+                        CheckStudentCount();
+                        break;
+                        case 3:
+                        return;
+                        break;
+                    default: 
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
+            }
+                  
+
+        
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public char Group { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, char group, int age)
+        {
+            Name = name;
+            Group = group;
+            Age = age;
+        }
+    }
+}
---
+++ Lesson 8/Lesson8task2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Lesson8task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accounts = new List<Account>();
+
+            // Keep adding accounts indefinitely
+            while (true)
+            {
+                AddAccount(accounts);
+            }
+
+            // This code won't be reached because of the infinite loop above
+        }
+
+        static void AddAccount(List<Account> accounts)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            // Validate user-entered name
+            if (!IsNameValid(name))
+            {
+                Console.WriteLine("Invalid name format. Only letters and numbers are allowed.");
+                return;
+            }
+
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            // Validate user-entered password
+            if (!IsPasswordValid(password))
+            {
+                Console.WriteLine("Invalid password format.");
+                return;
+            }
+
+            Account newAccount = new Account(name, password);
+            accounts.Add(newAccount);
+
+            foreach (var account in accounts)
+            {
+                Console.WriteLine($"Account Number: {account.Number}, Name: {account.Name}, Password: {account.Password}");
+            }
+        }
+
+        static bool IsNameValid(string name)
+        {
+            return Regex.IsMatch(name, @"^[a-zA-Z0-9]+$");
+        }
+
+        static bool IsPasswordValid(string password)
+        {
+            // Password validation: Length 8-25, at least 1 symbol, 1 uppercase letter, and 1 digit
+            return Regex.IsMatch(password, @"^(?=.*[!@#$%^&*(),.?""\:{}|<>])(?=.*[A-Z])(?=.*\d).{8,25}$");
+        }
+    }
+
+    class Account
+    {
+        private static int nextAccountNumber = 1; // Start with account number 1
+
+        public string Number { get; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public Account(string name, string password)
+        {
+            Number = nextAccountNumber.ToString(); // Assign the current account number
+            nextAccountNumber++; // Increment for the next account
+            Name = name; // Set the account holder's name
+            Password = password; // Set the account password
+        }
+    }
+}
